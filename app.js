baa8060b52b840c559ebfe1f1d12a42077b5202d
Abdullah30nov <maviazindaninfo@gmail.com>
2024-02-29 21:16:15 +0500

    JS-class-03 ARRAY code

---
+++ app.js
@@ -0,0 +1,31 @@
+// var name1 = "Abdullah"
+// var name2 = "Ali"
+// var name3 = "Ahmed"
+// var name4 = "ibrahim"
+// var name5 = "babar"
+
+// var names = ["Abdullah","Ali","Ahmed","ibrahim","babar"];
+//   console.log(names)
+//   document.write(names);
+
+//   names.splice(2,1,"junaid","jamshaid");
+//   names.slice(2,2);
+//   document.write(names);
+
+// for (var i = 20 ; i >= 0 ; i--,i--,i--,i--){
+//     document.write(i +"<br>");
+// }
+
+
+// var hd = "Qualifications";
+// var  edu1 = "HSC"; 
+// var edu2="SSC"; 
+// var edu3="BCS";
+// var edu4="BS"; 
+// var edu5="BCOM";
+// var edu6="MS"; 
+// var edu7="M. Phil"; 
+// var edu8="Phd";
+// var education = ("HSC","SSC","BCS","BS","BCOM","MS","M. Phil","Phd");
+// document.write("<br>" + education[hd] + "<br>" + education[edu1]);
+// // console.log(education + "<br>");
